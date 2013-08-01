17fb34fe169f91e4822519613858b1c1b99ecc1c
Sander van der Burg <svanderburg@gmail.com>
2013-08-01 15:10:48 +0200

    Implemented interesting way to configure and refer to api-levels and ABIs

--- deployment/default.nix
+++ deployment/default.nix
@@ -1,37 +1,73 @@
 { nixpkgs ? <nixpkgs>
 , system ? builtins.currentSystem
-, buildPlatformVersion ? "16"
-, emulatePlatformVersion ? "16"
-, abiVersion ? "armeabi-v7a"
+, buildPlatformVersions ? [ "16" ]
+, emulatePlatformVersions ? [ "16" ]
+, abiVersions ? [ "armeabi-v7a" ]
 }:
 
 let
   pkgs = import nixpkgs { inherit system; };
 in
 rec {
-  myfirstapp_debug = import ./myfirstapp {
-    inherit (pkgs) androidenv;
-    platformVersion = buildPlatformVersion;
-    release = false;
-  };
+  myfirstapp_debug = builtins.listToAttrs (map (buildPlatformVersion:
+    { name = "build_" + buildPlatformVersion;
+      value = import ./myfirstapp {
+        inherit (pkgs) androidenv;
+        platformVersion = buildPlatformVersion;
+        release = false;
+      };
+    }
+  ) buildPlatformVersions);
   
-  myfirstapp_release = import ./myfirstapp {
-    inherit (pkgs) androidenv;
-    platformVersion = buildPlatformVersion;
-    release = true;
-  };
+  myfirstapp_release = builtins.listToAttrs (map (buildPlatformVersion:
+    { name = "build_" + buildPlatformVersion;
+      value = import ./myfirstapp {
+        inherit (pkgs) androidenv;
+        platformVersion = buildPlatformVersion;
+        release = true;
+      };
+    }
+  ) buildPlatformVersions);
   
-  emulate_myfirstapp_debug = import ./emulate-myfirstapp {
-    inherit (pkgs) androidenv;
-    inherit abiVersion;
-    platformVersion = emulatePlatformVersion;
-    myfirstapp = myfirstapp_debug;
-  };
+  emulate_myfirstapp_debug = builtins.listToAttrs (map (buildPlatformVersion:
+    { name = "build_" + buildPlatformVersion;
+      value = builtins.listToAttrs (map (emulatePlatformVersion:
+      
+        { name = "emulate_" + emulatePlatformVersion;
+          value = builtins.listToAttrs (map (abiVersion:
+        
+            { name = abiVersion;
+              value = import ./emulate-myfirstapp {
+                inherit (pkgs) androidenv;
+                inherit abiVersion;
+                platformVersion = emulatePlatformVersion;
+                myfirstapp = builtins.getAttr "build_${buildPlatformVersion}" myfirstapp_debug;
+              };
+            }
+          ) abiVersions);
+        }
+      ) emulatePlatformVersions);
+    }
+  ) buildPlatformVersions);
   
-  emulate_myfirstapp_release = import ./emulate-myfirstapp {
-    inherit (pkgs) androidenv;
-    inherit abiVersion;
-    platformVersion = emulatePlatformVersion;
-    myfirstapp = myfirstapp_release;
-  };
+  emulate_myfirstapp_release = builtins.listToAttrs (map (buildPlatformVersion:
+    { name = "build_" + buildPlatformVersion;
+      value = builtins.listToAttrs (map (emulatePlatformVersion:
+      
+        { name = "emulate_" + emulatePlatformVersion;
+          value = builtins.listToAttrs (map (abiVersion:
+        
+            { name = abiVersion;
+              value = import ./emulate-myfirstapp {
+                inherit (pkgs) androidenv;
+                inherit abiVersion;
+                platformVersion = emulatePlatformVersion;
+                myfirstapp = builtins.getAttr "build_${buildPlatformVersion}" myfirstapp_release;
+              };
+            }
+          ) abiVersions);
+        }
+      ) emulatePlatformVersions);
+    }
+  ) buildPlatformVersions);
 }
