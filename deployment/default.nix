4542cae9432dee87f6492e56ff0e6d0378d3dac4
Sander van der Burg <svanderburg@gmail.com>
2013-12-24 15:45:53 +0100

    Converted composition expression to Hydra's new convention

--- deployment/default.nix
+++ deployment/default.nix
@@ -1,78 +1,101 @@
 { nixpkgs ? <nixpkgs>
-, system ? builtins.currentSystem
+, systems ? [ "x86_64-linux" ]
 , buildPlatformVersions ? [ "16" ]
 , emulatePlatformVersions ? [ "16" ]
 , abiVersions ? [ "armeabi-v7a" ]
 }:
 
 let
-  pkgs = import nixpkgs { inherit system; };
-  
-  # Allow strings as parameters by converting them to lists
-  _buildPlatformVersions = if builtins.isList buildPlatformVersions then buildPlatformVersions else [ buildPlatformVersions ];
-  _emulatePlatformVersions = if builtins.isList emulatePlatformVersions then emulatePlatformVersions else [ emulatePlatformVersions ];
-  _abiVersions = if builtins.isList abiVersions then abiVersions else [ abiVersions ];
+  pkgs = import nixpkgs {};
 in
 rec {
-  myfirstapp_debug = builtins.listToAttrs (map (buildPlatformVersion:
-    { name = "build_" + buildPlatformVersion;
-      value = import ./myfirstapp {
-        inherit (pkgs) androidenv;
-        platformVersion = buildPlatformVersion;
-        release = false;
-      };
-    }
-  ) _buildPlatformVersions);
-  
-  myfirstapp_release = builtins.listToAttrs (map (buildPlatformVersion:
-    { name = "build_" + buildPlatformVersion;
-      value = import ./myfirstapp {
-        inherit (pkgs) androidenv;
-        platformVersion = buildPlatformVersion;
-        release = true;
-      };
-    }
-  ) _buildPlatformVersions);
-  
-  emulate_myfirstapp_debug = builtins.listToAttrs (map (buildPlatformVersion:
-    { name = "build_" + buildPlatformVersion;
-      value = builtins.listToAttrs (map (emulatePlatformVersion:
-      
-        { name = "emulate_" + emulatePlatformVersion;
-          value = builtins.listToAttrs (map (abiVersion:
-        
-            { name = abiVersion;
-              value = import ./emulate-myfirstapp {
-                inherit (pkgs) androidenv;
-                inherit abiVersion;
-                platformVersion = emulatePlatformVersion;
-                myfirstapp = builtins.getAttr "build_${buildPlatformVersion}" myfirstapp_debug;
-              };
-            }
-          ) _abiVersions);
+  myfirstapp_debug = builtins.listToAttrs (map (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    { name = "host_"+system;
+      value = builtins.listToAttrs (map (buildPlatformVersion:
+        { name = "build_" + buildPlatformVersion;
+          value = import ./myfirstapp {
+            inherit (pkgs) androidenv;
+            platformVersion = buildPlatformVersion;
+            release = false;
+          };
         }
-      ) _emulatePlatformVersions);
+      ) buildPlatformVersions);
     }
-  ) _buildPlatformVersions);
+  ) systems);
   
-  emulate_myfirstapp_release = builtins.listToAttrs (map (buildPlatformVersion:
-    { name = "build_" + buildPlatformVersion;
-      value = builtins.listToAttrs (map (emulatePlatformVersion:
-      
-        { name = "emulate_" + emulatePlatformVersion;
-          value = builtins.listToAttrs (map (abiVersion:
-        
-            { name = abiVersion;
-              value = import ./emulate-myfirstapp {
-                inherit (pkgs) androidenv;
-                inherit abiVersion;
-                platformVersion = emulatePlatformVersion;
-                myfirstapp = builtins.getAttr "build_${buildPlatformVersion}" myfirstapp_release;
-              };
-            }
-          ) _abiVersions);
+  myfirstapp_release = builtins.listToAttrs (map (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    { name = "host_"+system;
+      value = builtins.listToAttrs (map (buildPlatformVersion:
+        { name = "build_" + buildPlatformVersion;
+          value = import ./myfirstapp {
+            inherit (pkgs) androidenv;
+            platformVersion = buildPlatformVersion;
+            release = true;
+          };
         }
-      ) _emulatePlatformVersions);
+      ) buildPlatformVersions);
     }
-  ) _buildPlatformVersions);
+  ) systems);
+  
+  emulate_myfirstapp_debug = builtins.listToAttrs (map (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    { name = "host_"+system;
+      value = builtins.listToAttrs (map (buildPlatformVersion:
+        { name = "build_" + buildPlatformVersion;
+          value = builtins.listToAttrs (map (emulatePlatformVersion:
+      
+            { name = "emulate_" + emulatePlatformVersion;
+              value = builtins.listToAttrs (map (abiVersion:
+        
+                { name = abiVersion;
+                  value = import ./emulate-myfirstapp {
+                    inherit (pkgs) androidenv;
+                    inherit abiVersion;
+                    platformVersion = emulatePlatformVersion;
+                    myfirstapp = builtins.getAttr "build_${buildPlatformVersion}" myfirstapp_debug;
+                  };
+                }
+              ) abiVersions);
+            }
+          ) emulatePlatformVersions);
+        }
+      ) buildPlatformVersions);
+    }
+  ) systems);
+  
+  emulate_myfirstapp_release = builtins.listToAttrs (map (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    { name = "host_"+system;
+      value = builtins.listToAttrs (map (buildPlatformVersion:
+        { name = "build_" + buildPlatformVersion;
+          value = builtins.listToAttrs (map (emulatePlatformVersion:
+      
+            { name = "emulate_" + emulatePlatformVersion;
+              value = builtins.listToAttrs (map (abiVersion:
+        
+                { name = abiVersion;
+                  value = import ./emulate-myfirstapp {
+                    inherit (pkgs) androidenv;
+                    inherit abiVersion;
+                    platformVersion = emulatePlatformVersion;
+                    myfirstapp = builtins.getAttr "build_${buildPlatformVersion}" myfirstapp_release;
+                  };
+                }
+              ) abiVersions);
+            }
+          ) emulatePlatformVersions);
+        }
+      ) buildPlatformVersions);
+    }
+  ) systems);
 }
