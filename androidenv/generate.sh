ad269ab5b0f1ff264126c5f713eea75b0bb4e008
Sander van der Burg <svanderburg@gmail.com>
2018-08-26 22:22:07 +0200

    Put XML sources in sub directory to keep structure clean

--- androidenv/generate.sh
+++ androidenv/generate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 
-curl https://dl.google.com/android/repository/repository2-1.xml -o repository2-1.xml
-curl https://dl.google.com/android/repository/sys-img/android/sys-img2-1.xml -o sys-img2-1.xml
-curl https://dl.google.com/android/repository/addon2-1.xml -o addon2-1.xml
+curl https://dl.google.com/android/repository/repository2-1.xml -o xml/repository2-1.xml
+curl https://dl.google.com/android/repository/sys-img/android/sys-img2-1.xml -o xml/sys-img2-1.xml
+curl https://dl.google.com/android/repository/addon2-1.xml -o xml/addon2-1.xml
 
-xsltproc convertpackages.xsl repository2-1.xml > generated/packages.nix
-xsltproc convertsystemimages.xsl sys-img2-1.xml > generated/system-images.nix
-xsltproc convertaddons.xsl addon2-1.xml > generated/addons.nix
+xsltproc convertpackages.xsl xml/repository2-1.xml > generated/packages.nix
+xsltproc convertsystemimages.xsl xml/sys-img2-1.xml > generated/system-images.nix
+xsltproc convertaddons.xsl xml/addon2-1.xml > generated/addons.nix
