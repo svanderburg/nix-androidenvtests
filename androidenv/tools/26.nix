1efbcf0ab5333d9d948cff0dfb2c8424c236ffe1
Sander van der Burg <svanderburg@gmail.com>
2018-11-07 22:48:30 +0100

    Make both 26.0.1 and 26.1.1 versions of the tools plugin work

--- androidenv/tools/26.nix
+++ androidenv/tools/26.nix
@@ -1,28 +1,13 @@
-{deployAndroidPackage, requireFile, lib, autopatchelf, makeWrapper, os, pkgs, pkgs_i686, postInstall ? ""}:
+{deployAndroidPackage, lib, package, autopatchelf, makeWrapper, os, pkgs, pkgs_i686, postInstall ? ""}:
 
 deployAndroidPackage {
   name = "androidsdk";
-  inherit os;
+  inherit os package;
   buildInputs = [ autopatchelf makeWrapper ];
 
   libs_x86_64 = lib.optionalString (os == "linux") (lib.makeLibraryPath [ pkgs.glibc pkgs.xlibs.libX11 pkgs.xlibs.libXrender pkgs.xlibs.libXext pkgs.fontconfig pkgs.freetype ]);
   libs_i386 = lib.optionalString (os == "linux") (lib.makeLibraryPath [ pkgs_i686.glibc pkgs_i686.xlibs.libX11 pkgs_i686.xlibs.libXrender pkgs_i686.xlibs.libXext pkgs_i686.fontconfig pkgs_i686.freetype pkgs_i686.zlib ]);
 
-  package = {
-    name = "tools";
-    path = "tools";
-    revision = "26.0.1";
-    archives = {
-      linux = requireFile {
-        url = https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip;
-        sha256 = "185yq7qwxflw24ccm5d6zziwlc9pxmsm3f54pm9p7xm0ik724kj4";
-      };
-      macosx = requireFile {
-        url = https://dl.google.com/android/repository/sdk-tools-darwin-3859397.zip;
-        sha256 = "1ycx9gzdaqaw6n19yvxjawywacavn1jc6sadlz5qikhgfr57b0aa";
-      };
-    };
-  };
   patchInstructions = ''
     ${lib.optionalString (os == "linux") ''
       # Auto patch all binaries
